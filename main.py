53e825f1e4a6cad830f59bfe5c45f091c46524e5
ofiszer <cc557639@gmail.com>
2024-05-31 19:24:33 +0200

    2nd commit
    added ball reseting position after missing paddles

--- main.py
+++ main.py
@@ -18,10 +18,26 @@ screen.onkey(r_paddle.go_up, "Up")
 screen.onkey(r_paddle.go_down, "Down")
 screen.onkey(l_paddle.go_up, "w")
 screen.onkey(l_paddle.go_down, "s")
-game_is_on=True
+game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
     ball.move()
+
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_y()
+
+    #paddle bounce
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
+        ball.bounce_x()
+
+    #missing r_paddle
+    if ball.xcor() > 380:
+        ball.reset_position()
+
+    #missing l_paddle
+    if ball.xcor() < -380:
+        ball.reset_position()
+
 
 screen.exitonclick()
